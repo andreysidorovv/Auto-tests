814fc8923c0af496368d8888de17c9a132d47197
andreysidorovv <sidorov.andrey2005@mail.ru>
2024-05-24 14:13:35 +0300

    Fixed issues with POM

--- POM/pages/basepage.js
+++ POM/pages/basepage.js
@@ -1,10 +1,10 @@
-const { Builder, Browser } = require('selenium-webdriver');
+const { Builder, Browser, By } = require('selenium-webdriver');
 
 class BasePage {
 
   async goToUrl(url) {
     global.driver = new Builder().forBrowser(Browser.CHROME).build();
-    driver.manage().setTimeouts({implicit: 5000});
+    driver.manage().setTimeouts({ implicit: 5000 });
     await driver.get(url);
   }
 
@@ -13,13 +13,27 @@ class BasePage {
   }
 
   async click(locator) {
-    await driver.findElement(locator).click();
+    const element = await driver.findElement(locator);
+    
+    const isDisplayedBefore = await element.isDisplayed();
+    const isEnabledBefore = await element.isEnabled();
+
+    if (isDisplayedBefore && isEnabledBefore) {
+      await element.click();
+      
+      const isDisplayedAfter = await element.isDisplayed();
+      const isEnabledAfter = await element.isEnabled();
+
+      console.log(`Element state before click: displayed=${isDisplayedBefore}, enabled=${isEnabledBefore}`);
+      console.log(`Element state after click: displayed=${isDisplayedAfter}, enabled=${isEnabledAfter}`);
+    } else {
+      throw new Error('Element not in clickable state');
+    }
   }
 
-  async SaveScreenshot(filename) {
-    driver.takeScreenshot().then(function (image){
-      require('fs').writeFileSync(fileName, image, 'base64')
-    });
+  async saveScreenshot(filename) {
+    const image = await driver.takeScreenshot();
+    require('fs').writeFileSync(filename, image, 'base64');
   }
 
   async closeBrowser() {
@@ -28,4 +42,4 @@ class BasePage {
   }
 }
 
-module.exports = BasePage
+module.exports = BasePage;
